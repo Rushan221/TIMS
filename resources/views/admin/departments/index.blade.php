7ab0701d65bd0a0e804f472786800d9332b6e5e6
Rushan <rushan2072@gmail.com>
2019-08-31 22:00:12 +0545

    [UPDATE] Index page revamped

--- resources/views/admin/departments/index.blade.php
+++ resources/views/admin/departments/index.blade.php
@@ -7,9 +7,9 @@
         </div>
         <div class="card-body">
             <div style="padding-bottom: 1em">
-                <a href="{{ route('admin.departments.create') }}" class="btn btn-success"><i class="fa fa-plus"></i> Add</a>
+                <a href="{{ route('admin.departments.create') }}" class="btn btn-success" title="Add"><i class="fa fa-plus"></i></a>
             </div>
-            <table class="table">
+            <table class="table table-striped">
                 <thead>
                     <th>Name</th>
                     <th>Code</th>
@@ -22,11 +22,11 @@
                         <td>{{ $department->code }}</td>
                         <td>
                             <div class="row">
-                                <a href="{{ route('admin.departments.edit',$department->id) }}" class="btn btn-primary btn-sm">Edit</a>&nbsp;                                
+                                <a href="{{ route('admin.departments.edit',$department->id) }}" title="Edit" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>&nbsp;                                
                                 <form action="{{ route('admin.departments.destroy',$department->id) }}" method="post">
                                     @method('Delete')
                                     @csrf
-                                    <button type="submit" class="btn btn-danger btn-sm" onclick="confirm('Are you sure you want to delete?')">Delete</button>
+                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete" onclick="return confirm('Are you sure you want to delete?')"><i class="fas fa-trash"></i></button>
                                 </form>
                             </div>
                         </td>
